5ce78d76ca3705dd6374c39742a1f9c74a441fa3
langonginc <langonginc@yeah.net>
2021-03-07 10:45:17 +0800

    upd

--- T/72672.cpp
+++ T/72672.cpp
@@ -9,8 +9,44 @@ int adj[inf][inf];
 int n;
 int dp[inf];
 
+int dfs (int x)
+{
+    if (dp[x] > 0)
+    {
+        return dp[x];
+    }
+    dp[x] = 1;
+    for (int i = 1; i <= n; i ++)
+    {
+        if (adj[x][i])
+        {
+            dp[x] = max (dp[x], dfs(i) + 1);
+        }
+    }
+    return dp[x];
+}
+
 int main ()
 {
     scanf ("%d", &n);
-    for (int i = 1; i <= n)
+    for (int i = 1; i <= n; i ++)
+    {
+        int x, y;
+        scanf ("%d%d", &x, &y);
+        adj[x][y] = 1;
+    }
+    for (int i = 1; i <= n; i ++)
+    {
+        if (dp[i] == 0)
+        {
+            dfs (i);
+        }
+    }
+    int mx = 0;
+    for (int i = 1; i <= n; i ++)
+    {
+        mx = max (mx, dp[i]);
+    }
+    printf ("%d\n", mx);
+    return 0;
 }
