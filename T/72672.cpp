55365e6e41f98e20214715a03c63a55ed470fdff
langonginc <langonginc@yeah.net>
2021-03-07 10:30:10 +0800

    T

---
+++ T/72672.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+using namespace std;
+const int inf = 1005;
+
+int adj[inf][inf];
+int n;
+int dp[inf];
+
+int main ()
+{
+    scanf ("%d", &n);
+    for (int i = 1; i <= n)
+}
