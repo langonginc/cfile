d9f424ad0315488d23e65ad2aaecc0bdc8a201c9
langonginc <langonginc@yeah.net>
2021-07-12 21:58:52 +0800

    高精度Update

--- P/1601.cpp
+++ P/1601.cpp
@@ -2,7 +2,7 @@
 #include <cstdio>
 #include <cstring>
 using namespace std;
-const int inf = 5005;
+const int inf = 1005;
 
 char s1[inf], s2[inf];
 int a[inf], b[inf], c[inf];
@@ -11,7 +11,7 @@ int main ()
 {
     cin >> s1 >> s2;
     int len1 = strlen(s1), len2 = strlen(s2);
-    int len = len1 + len2;
+    int len = max (len1, len2);
     for (int i = 0; i < len1; i ++)
     {
         a[i] = s1[len1 - i - 1] - '0';
@@ -20,15 +20,9 @@ int main ()
     {
         b[i] = s2[len2 - i - 1] - '0';
     }
-    for (int i = 0; i < len1; i ++)
+    for (int i = 0; i < len; i ++)
     {
-        for (int j = 0; j < len2; j ++)
-        {
-            c[i + j] += a[i] * b[j];
-        }
-    }
-    for (int i = 0; i < len; i++)
-    {
+        c[i] += a[i] + b[i];
         c[i + 1] += c[i] / 10;
         c[i] %= 10;
     }
--- P/2142.cpp
+++ P/2142.cpp
@@ -2,16 +2,23 @@
 #include <cstdio>
 #include <cstring>
 using namespace std;
-const int inf = 5005;
+const int inf = 10086 + 10;
 
 char s1[inf], s2[inf];
 int a[inf], b[inf], c[inf];
+bool positive = true;
 
 int main ()
 {
     cin >> s1 >> s2;
     int len1 = strlen(s1), len2 = strlen(s2);
-    int len = len1 + len2;
+    int len = max (len1, len2);
+    if (len1 < len2 || (len1 == len2 && strcmp(s1, s2) < 0))
+    {
+        positive = false;
+        swap (s1, s2);
+        swap (len1, len2);
+    }
     for (int i = 0; i < len1; i ++)
     {
         a[i] = s1[len1 - i - 1] - '0';
@@ -20,18 +27,15 @@ int main ()
     {
         b[i] = s2[len2 - i - 1] - '0';
     }
-    for (int i = 0; i < len1; i ++)
+    for (int i = 0; i < len; i ++)
     {
-        for (int j = 0; j < len2; j ++)
+        if (a[i] < b[i])
         {
-            c[i + j] += a[i] * b[j];
+            a[i + 1] --, a[i] += 10;
         }
+        c[i] = a[i] - b[i];
     }
-    for (int i = 0; i < len; i++)
-    {
-        c[i + 1] += c[i] / 10;
-        c[i] %= 10;
-    }
+    if (!positive) printf ("-");
     while (c[len] == 0 && len > 0) len --;
     while (len >= 0) printf ("%d", c[len --]);
     return 0;
