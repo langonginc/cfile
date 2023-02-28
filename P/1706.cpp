d63de5b5bffb6baa19376224b99f277f00264d36
langonginc <langonginc@yeah.net>
2023-02-28 21:10:44 +0800

    upd

--- P/1706.cpp
+++ P/1706.cpp
@@ -1,31 +1,38 @@
-#include<iostream>
-#include<cstdio>
+#include <iostream>
+#include <cstdio>
 using namespace std;
-int n,a[1000];
+int n, a[1000];
 bool f[1000];
-void try_(int d){
-	if(d>n){
-		for(int i=1;i<n+1;i++){
+void dfs (int d)
+{
+	if (d > n)
+	{
+		for (int i = 1; i < n + 1; i ++)
+		{
 			printf("%5d",a[i]+1);
 		}
 		printf("\n");
 		return;
 	}
-	for(int i=0;i<n;i++){
-		if(f[i]){
-			a[d]=i;
-			f[i]=false;
-			try_(d+1);
-			f[i]=true;
+	for (int i = 0; i < n; i ++)
+	{
+		if (f[i])
+		{
+			a[d] = i;
+			f[i] = false;
+			dfs (d + 1);
+			f[i] = true;
 		}
 	}
 }
-int main(){
-	scanf("%d",&n);
-	for(int i=0;i<n;i++){
-		a[i]=0;
-		f[i]=true;
+int main()
+{
+	scanf ("%d", &n);
+	for (int i = 0; i < n; i ++)
+	{
+		a[i] = 0;
+		f[i] = true;
 	}
-	try_(1);
+	dfs (1);
 	return 0;
 }
