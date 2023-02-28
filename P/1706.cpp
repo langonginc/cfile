4ede0068f65f02d74ad9eea2383625300a512feb
langonginc <langonginc@yeah.net>
2023-02-28 21:15:03 +0800

    upd

--- P/1706.cpp
+++ P/1706.cpp
@@ -2,37 +2,32 @@
 #include <cstdio>
 using namespace std;
 int n, a[1000];
-bool f[1000];
+bool vis[1000];
 void dfs (int d)
 {
 	if (d > n)
 	{
-		for (int i = 1; i < n + 1; i ++)
+		for (int i = 1; i <= n; i ++)
 		{
-			printf("%5d",a[i]+1);
+			printf ("%5d", a[i]);
 		}
-		printf("\n");
+		printf ("\n");
 		return;
 	}
-	for (int i = 0; i < n; i ++)
+	for (int i = 1; i <= n; i ++)
 	{
-		if (f[i])
+		if (vis[i] == false)
 		{
 			a[d] = i;
-			f[i] = false;
+			vis[i] = true;
 			dfs (d + 1);
-			f[i] = true;
+			vis[i] = false;
 		}
 	}
 }
 int main()
 {
 	scanf ("%d", &n);
-	for (int i = 0; i < n; i ++)
-	{
-		a[i] = 0;
-		f[i] = true;
-	}
 	dfs (1);
 	return 0;
 }
